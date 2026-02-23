b4d7523828b8cb2dd52eb00c72e177ab928795b7
vemis <micsindler@gmail.com>
2026-02-23 21:35:48 +0100

    MongoDB Mongoose JavaScript/Node.js - Collections, loading of most collections

---
+++ MongoDB/JavaScript-Node.jsODMs/mongoose-project/index-r.js
@@ -0,0 +1,59 @@
+import mongoose, { mongo } from "mongoose";
+
+// setting off the letter s on the end of a collection
+// probably have no meaning -> needs to be called before the initialization
+// that already happened in the tpc_h_r folder
+mongoose.pluralize(null)
+
+import RegionR from "./models/tpc_h_r/region-r.js";
+import NationR from "./models/tpc_h_r/nation-r.js";
+import CustomerR from "./models/tpc_h_r/customer-r.js";
+import OrdersR from "./models/tpc_h_r/orders-r.js";
+import PartR from "./models/tpc_h_r/part-r.js";
+import PartsuppR from "./models/tpc_h_r/partsupp-r.js";
+import SupplierR from "./models/tpc_h_r/supplier-r.js";
+import LineitemR from "./models/tpc_h_r/lineitem-r.js";
+
+import * as loadDataTPCH from "./load-data-tpc-h.js";
+
+async function run() {
+    // Connect to MongoDB
+    await mongoose.connect("mongodb://localhost:27017/mongoose_database_r");
+    
+    console.log("Connected to MongoDB.");
+    
+    //await loadDataTPCH.loadRegions("..\\..\\..\\dataset\\TPC-H\\tpch-data\\region.tbl")
+    //console.log("Regions loaded")
+
+    //await loadDataTPCH.loadNations("..\\..\\..\\dataset\\TPC-H\\tpch-data\\nation.tbl")
+    //console.log("Nations loaded")
+    
+    //await loadDataTPCH.loadCustomers("..\\..\\..\\dataset\\TPC-H\\tpch-data\\customer.tbl")
+    //console.log("Customers loaded")
+
+    //await loadDataTPCH.loadOrders("..\\..\\..\\dataset\\TPC-H\\tpch-data\\orders.tbl")
+    //console.log("Orders loaded");
+
+    //console.log("Loading Lineitems")
+    //await loadDataTPCH.loadLineitems("..\\..\\..\\dataset\\TPC-H\\tpch-data\\lineitem.tbl")
+    //console.log("Lineitems loaded");
+/*
+    // Insert Joe Doe
+    const customer1 = await CustomerR.insertMany([
+        {
+            _id: 1,
+            c_name: 'Customer#000000001',
+            c_address: 'IVhzIApeRb ot,c,E',
+            c_nationkey: 15,
+            c_phone: '25-989-741-2988',
+            c_acctbal: 711.56,
+            c_mktsegment: 'BUILDING',
+            c_commen: 'to the even, regular platelets. regular, ironic epitaphs nag e'
+        }
+
+    ]);
+    console.log("customers saved")
+*/
+}
+
+run().catch((err) => console.error(err));
---
+++ MongoDB/JavaScript-Node.jsODMs/mongoose-project/load-data-tpc-h.js
@@ -0,0 +1,204 @@
+import fs from 'fs';
+
+import RegionR from "./models/tpc_h_r/region-r.js";
+import NationR from "./models/tpc_h_r/nation-r.js";
+import CustomerR from "./models/tpc_h_r/customer-r.js";
+import OrdersR from "./models/tpc_h_r/orders-r.js";
+import PartR from "./models/tpc_h_r/part-r.js";
+import PartsuppR from "./models/tpc_h_r/partsupp-r.js";
+import SupplierR from "./models/tpc_h_r/supplier-r.js";
+import LineitemR from "./models/tpc_h_r/lineitem-r.js";
+
+async function readDataFromCustomSeparator(filePath){
+    const data = await fs.promises.readFile(filePath, 'utf8');
+    var rows = data.split('\n');
+
+    // the last one is just empty string
+    if (rows.length > 1 && rows[rows.length - 1] == '') {
+        rows.pop()
+    }
+    return rows.map(row => row.split('|'));
+}
+
+async function loadRegions(filePath){
+    try {
+        const rowsOfData = await readDataFromCustomSeparator(filePath)
+        
+        const rowsOfSchemas = rowsOfData.map(([_id, r_name, r_comment]) => ({
+            _id: Number(_id),
+            r_name,
+            r_comment
+        }));
+
+        //console.log(rowsOfSchemas)
+
+        await RegionR.insertMany( rowsOfSchemas );
+
+    } catch (err) {
+        console.error(err);
+    }
+
+}
+
+async function loadNations(filePath){
+    try {
+        const rowsOfData = await readDataFromCustomSeparator(filePath)
+        
+        const rowsOfSchemas = rowsOfData.map(([_id, n_name, n_regionkey, r_comment]) => ({
+            _id: Number(_id),
+            n_name,
+            n_regionkey,
+            r_comment
+        }));
+
+        console.log(rowsOfSchemas)
+
+        await NationR.insertMany( rowsOfSchemas );
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function loadCustomers(filePath){
+    try {
+        const rowsOfData = await readDataFromCustomSeparator(filePath)
+        
+        const rowsOfSchemas = rowsOfData.map(([
+            _id,
+            c_name,
+            c_address,
+            c_nationkey,
+            c_phone,
+            c_acctbal,
+            c_mktsegment,
+            c_commen ]) => ({
+                _id: Number(_id),
+                c_name,
+                c_address,
+                c_nationkey: Number(c_nationkey),
+                c_phone,
+                c_acctbal: Number(c_acctbal),
+                c_mktsegment,
+                c_commen
+            })
+        );
+
+        await CustomerR.insertMany( rowsOfSchemas );
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function loadOrders(filePath){
+    try {
+        const rowsOfData = await readDataFromCustomSeparator(filePath)
+
+        const rowsOfSchemas = rowsOfData.map(([
+                 _id,
+                 o_custkey,
+                 o_orderstatus,
+                 o_totalprice,
+                 o_orderdate,
+                 o_orderpriority,
+                 o_clerk,
+                 o_shippriority,
+                 o_comment
+                 ]) => ({
+                    _id: Number(_id),
+                    o_custkey: Number(o_custkey),
+                    o_orderstatus,
+                    o_totalprice,
+                    o_orderdate: Date(o_orderdate),
+                    o_orderpriority,
+                    o_clerk,
+                    o_shippriority,
+                    o_comment
+                })
+        );
+
+        await OrdersR.insertMany( rowsOfSchemas );
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+async function loadLineitems(filePath){
+    try {
+        const rowsOfData = await readDataFromCustomSeparator(filePath)
+
+        const rowsOfSchemas = rowsOfData.map(([
+                                                  l_orderkey,
+                                                  l_partkey,
+                                                  l_suppkey,
+                                                  l_linenumber,
+                                                  l_quantity,
+                                                  l_extendedprice,
+                                                  l_discount,
+                                                  l_tax,
+                                                  l_returnflag,
+                                                  l_linestatus,
+                                                  l_shipdate,
+                                                  l_commitdate,
+                                                  l_receiptdate,
+                                                  l_shipinstruct,
+                                                  l_shipmode,
+                                                  l_comment
+                                              ]) => ({
+            _id: l_orderkey + "|" + l_linenumber,
+            l_orderkey: Number(l_orderkey),
+            l_partkey: Number(l_partkey),
+            l_suppkey: Number(l_suppkey),
+            l_ps_id: l_partkey + "|" + l_suppkey,
+            l_linenumber: Number(l_linenumber),
+            l_quantity: Number(l_quantity),
+            l_extendedprice: Number(l_extendedprice),
+            l_discount: Number(l_discount),
+            l_tax: Number(l_tax),
+            l_returnflag,
+            l_linestatus,
+            l_shipdate,
+            l_commitdate,
+            l_receiptdate,
+            l_shipinstruct,
+            l_shipmode,
+            l_comment
+            })
+        );
+
+        const rowsOfSchemasBatches = partition(rowsOfSchemas, 100_000);
+
+        for (let i = 0; i < rowsOfSchemasBatches.length; i++) {
+            await LineitemR.insertMany( rowsOfSchemasBatches[i] );
+            console.log("Batch inserted!")
+        }
+
+
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+function partition(array, batchSize) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i += batchSize) {
+        result.push(array.slice(i, i + batchSize));
+    }
+
+    return result;
+}
+
+// exported API
+export {
+    loadRegions,
+    loadNations,
+    loadCustomers,
+    loadOrders,
+    loadLineitems
+}
+
+
+
