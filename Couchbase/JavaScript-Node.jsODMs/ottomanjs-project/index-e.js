444dce47a1171b74980a50652ff47c50c7f406b3
vemis <micsindler@gmail.com>
2026-03-13 11:22:34 +0100

    Couchbase Ottoman.js JavaScript Embedded model - creation, indexing inside of an array not working to date

---
+++ Couchbase/JavaScript-Node.jsODMs/ottomanjs-project/index-e.js
@@ -0,0 +1,40 @@
+// this stupid import is the only way how I was able to use the Ottoman.js
+import {} from './config/db-e.js';
+
+import ottoman from "ottoman";
+import OrdersESchema from "./models/tpc_h_e/orders-e.js";
+import {CustomerEWithOrders} from "./models/tpc_h_e/customer-e-with-orders.js";
+
+
+async function run(){
+    const cuse1 = new CustomerEWithOrders({
+        id: "1",
+        c_name: "test",
+        c_address: "test",
+
+        c_nationkey: "1",
+        c_phone: "test",
+        c_acctbal: 123,
+        c_mktsegment: "test",
+        c_commen: "test",
+
+        c_orders: [
+            {
+                o_orderkey: "1",
+                o_custkey: "1",
+                o_orderstatus: "test",
+                o_totalprice: 123,
+                o_orderdate: new Date("1993-01-01"),
+                o_orderpriority: "test",
+                o_clerk: "test",
+                o_shippriority: "test",
+                o_comment:"test"
+            }
+        ]
+    });
+
+    await cuse1.save();
+
+}
+
+run().catch(err => console.error(err));
