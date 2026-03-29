87ea40da065917e99b6c8e0522d53c9ad3f2d389
vemis <micsindler@gmail.com>
2026-03-29 22:50:44 +0200

    bug reports in models - string -> double in orders, key-lower-upper-cases in fields of partsupp

--- MongoDB/JavaODMs/odmstest/src/main/java/cz/cuni/mff/mongodb_java/springdata_r/model/PartsuppR.java
+++ MongoDB/JavaODMs/odmstest/src/main/java/cz/cuni/mff/mongodb_java/springdata_r/model/PartsuppR.java
@@ -9,9 +9,9 @@ public class PartsuppR {
     @Id
     private String ps_id;
     @Indexed
-    private int ps_partKey;
+    private int ps_partkey;
     @Indexed
-    private int ps_suppKey;
+    private int ps_suppkey;
 
     private int ps_availqty;
     private double ps_supplycost;
@@ -19,10 +19,10 @@ public class PartsuppR {
 
     public PartsuppR() {}
 
-    public PartsuppR( int ps_partKey, int ps_suppKey, int ps_availqty, double ps_supplycost, String ps_comment) {
-        this.ps_id = Integer.toString(ps_partKey) + "|" + Integer.toString(ps_suppKey);
-        this.ps_partKey = ps_partKey;
-        this.ps_suppKey = ps_suppKey;
+    public PartsuppR(int ps_partkey, int ps_suppkey, int ps_availqty, double ps_supplycost, String ps_comment) {
+        this.ps_id = Integer.toString(ps_partkey) + "|" + Integer.toString(ps_suppkey);
+        this.ps_partkey = ps_partkey;
+        this.ps_suppkey = ps_suppkey;
         this.ps_availqty = ps_availqty;
         this.ps_supplycost = ps_supplycost;
         this.ps_comment = ps_comment;
